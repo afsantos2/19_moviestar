884a9627648c6337c998f091d8d840286830d8bd
afsantos2 <anderson_f.ads@hotmail.com>
2024-07-22 00:22:29 -0300

    gravando msg nos cookies

--- auth.php
+++ auth.php
@@ -22,7 +22,7 @@ require_once('templates/header.php');
 
         <div id="register-container" class="col-md-4">
           <h2>Criar conta</h2>
-          <form action="" method="POST">
+          <form action="<?=$BASE_URL?>auth_process.php" method="POST">
             <input type="hidden" name="type" value="register">
             <div class="form-group">
               <label for="email">E-mail:</label>
