e473bb031202a98e03c13ce36f8aa313eaff79a1
afsantos2 <anderson_f.ads@hotmail.com>
2024-10-09 00:02:12 -0300

    funcao padrao de debug

--- globals.php
+++ globals.php
@@ -2,4 +2,9 @@
 session_start();
 
 $BASE_URL = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI'] . '?');
-?>
+
+function pr($dados) {
+  echo '<pre>';
+  print_r($dados);
+  echo '</pre>';
+}
