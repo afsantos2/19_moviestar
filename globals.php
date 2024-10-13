0d8845cc6cf8fc475f603936d938b6c5b7ef2691
afsantos2 <anderson_f.ads@hotmail.com>
2024-10-12 23:59:34 -0300

    add funcao de log

--- globals.php
+++ globals.php
@@ -8,3 +8,13 @@ function pr($dados) {
   print_r($dados);
   echo '</pre>';
 }
+
+// Função para registrar uma mensagem no log
+function _log($mensagem) {
+  $arquivo_log = 'logs/meu_log.log';
+  $data_atual = date('Y-m-d H:i:s');
+  $mensagem_formatada = "[{$data_atual}] {$mensagem}\n\n";
+
+  file_put_contents($arquivo_log, $mensagem_formatada, FILE_APPEND | LOCK_EX);
+}
+?>
